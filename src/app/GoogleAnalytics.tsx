4d67d9944b8abb82fea1a22a4aed6b638ab8f59a
Joshua Ansah <joshua.ansah@digitalinspirationlabs.io>
2023-12-04 22:54:47 +0000

    feat: google analytics

--- src/app/GoogleAnalytics.tsx
+++ src/app/GoogleAnalytics.tsx
@@ -1,36 +1,29 @@
+"use client";
 
-'use client'
-
-import Script from "next/script"
-import * as gtag from "../../gtag"
+import Script from "next/script";
+import * as gtag from "../../gtag";
 
 const GoogleAnalytics = () => {
+  //You can show in the console the GA_TRACKING_ID to confirm
+  console.log(gtag.GA_TRACKING_ID);
 
-    //You can show in the console the GA_TRACKING_ID to confirm
-    console.log(gtag.GA_TRACKING_ID)
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${gtag.GA_TRACKING_ID}');
+        `}
+      </Script>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-            />
-            <Script
-                id="gtag-init"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-                      gtag('config', '${gtag.GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                      });
-                    `,
-                }}
-            />
-        </>
-    )
-}
-
-export default GoogleAnalytics
+export default GoogleAnalytics;
