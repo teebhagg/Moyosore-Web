01f823693b5ca462a566ef3da57f081c627182c0
Joshua Ansah <joshua.ansah@digitalinspirationlabs.io>
2023-12-04 23:18:42 +0000

    feat: meta tags

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,19 @@
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About | Moyosore',
+  description: 'Explore the vibrant world of Fellowship, Medicine, Creativity, and People through the lens of an eccentric individual with a unique perspective on life.',
+}
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
