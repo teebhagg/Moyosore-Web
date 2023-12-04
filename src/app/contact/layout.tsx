d5e1c07c5976d59081da88239d58036879ad1b22
Joshua Ansah <joshua.ansah@digitalinspirationlabs.io>
2023-12-04 23:35:47 +0000

    feat: meta tags

--- src/app/contact/layout.tsx
+++ src/app/contact/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Contact | Moyosore',
-  description: 'Explore the vibrant world of Fellowship, Medicine, Creativity, and People through the lens of an eccentric individual with a unique perspective on life.',
+  description: 'Explore the vibrant world of Fellowship, Medicine, Creativity, and People through the lens of an eccentric individual with a unique perspective on life, My name is Moyosore and here is my God-Sized vision',
 }
 
 export default function ContactLayout({
