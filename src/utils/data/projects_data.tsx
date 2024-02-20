4aed98a3c48b604780343b55f410f701d53e1169
Joshua Ansah <joshua.ansah@digitalinspirationlabs.io>
2024-02-20 18:52:01 +0000

    refactor: reorder projects

--- src/utils/data/projects_data.tsx
+++ src/utils/data/projects_data.tsx
@@ -2,7 +2,7 @@ import { client } from "../../../sanity/client";
 import { ProjectsPeekInterface } from "../interface/home";
 
 export const getProjects = async () => {
-  const projectQuery = `*[_type == 'projects']`;
+  const projectQuery = `*[_type == 'projects'] | order(_createdAt desc)`;
   const projectData: ProjectsPeekInterface[] = await client.fetch(projectQuery);
   console.log(projectData);
   return { projectData };
